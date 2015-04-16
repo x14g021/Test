937e5fdfb5c0220edc6ca5973d4c96a94f4e9243
x14g021 <x14g021@PC505N020.chiba-fjb.ac.jp>
2015-04-16 15:04:16 +0900

    ABC

---
+++ src/java_2G_1/Test01.java
@@ -0,0 +1,51 @@
+package java_2G_1;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		
+//		int a = 10;
+//		float b = 100.5f;
+//		double c = 1000.15;
+//		char d = 'M';
+//		String e = "ふぉ";
+//
+//		String s = String.format("整数:%05d\n小数:%4.3f\n小数:%f\n文字:%c\n文字列:%s\n",a,b,c,d,e);
+////		System.out.println(s);
+		
+//		String s = "AbCdE";
+//		//小文字
+//		System.out.println(s.toLowerCase());
+//		//大文字
+//		System.out.println(s.toUpperCase());
+		
+//		String s = "ABCDEFABCDEF";
+//		//CDをXYに置換
+//		String s2 = s.replaceAll("CD", "XY");
+//		System.out.println(s2);
+		
+//		System.out.print("こんにち\nは世界\n");
+//		System.out.flush();
+//		System.err.print("こんにちは世界2\\n");
+		
+//		int i;
+//		//iを0に初期設定;i<3を満たしていたら処理を実行;処理終了後、iに1を足す
+//		for(i=0;i<3;i=i+1)  
+//		    System.out.println(i);  //iの値を出力し改行
+		
+//		int i;
+//		int a = 0;
+//		for(i=0;i<3;i++)  
+//		    a = a+i;
+		
+//		int[] a = {5,8,2,1,4};
+//		int i;
+//		for(i=0;i<a.length;i++)
+//		{
+//		    System.out.println(a[i]);
+//		}
+		
+
+	}
+
+}
